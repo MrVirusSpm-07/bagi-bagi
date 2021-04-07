f3df890773c119edaf45dec3dde8c6039adfdba0
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-04-07 23:25:48 +0700

    Add files via upload

---
+++ scdfc.sh
@@ -0,0 +1,99 @@
+#MRVIRUSSPM
+#PASWORD ; SUBSCRIBE GAN
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+sleep 1
+echo $blue
+echo "//////////////////////////////////////////////////////"
+echo $red" ____   ____      ____  _____ _____ _    ____ _____  "
+echo "/ ___| / ___|    |  _ \| ____|  ___/ \  / ___| ____| "
+echo "\___ \| |   _____| | | |  _| | |_ / _ \| |   |  _|   "
+echo " ___) | |__|_____| |_| | |___|  _/ ___ \ |___| |___  "
+echo "|____/ \____|    |____/|_____|_|/_/   \_\____|_____| "
+echo $blue"//////////////////////////////////////////////////////"
+echo $green
+sleep 1
+echo "::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "(#)   Author    : MR VIRUS SPM              (#)"
+echo "(#)   Team      : WHITE CYBER UNION         (#)"
+echo "(#)   Script    : 100 SCRIPT DEFACE         (#)"
+echo "::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo $red
+sleep 2
+read -p "MASUKAN PASSWORD DULU => " hoo
+
+if [ $hoo = 5555 ] || [ $hoo = 5555 ]
+then
+clear
+sleep 2
+echo $yellow
+echo "    SILAHKAN ANDA COPY LALU PASTE KE CROOM"
+echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
+echo $white
+echo "1. https://www.mediafire.com/file/j1mn4tkqh9bkq0r/Indonesia_defacer.html/file"
+sleep 1
+echo "2. https://www.mediafire.com/file/4ojk6g41kx6b4cw/INDONESIA_45.html/file"
+sleep 1
+echo
+echo "3. https://www.mediafire.com/file/o9djwbbkxvznmr0/Anjay.html/file"
+sleep 1
+echo
+echo "4. https://www.mediafire.com/file/hqnmcoo1e0dvekg/KineFreedomSc.html/file"
+sleep 1
+echo
+echo "5. https://www.mediafire.com/file/fqb8y74myhkrlin/index.html/file"
+sleep 1
+echo
+echo "6. https://www.mediafire.com/file/p1ppar36u8f969q/tikusberdasi.html/file"
+sleep 1
+echo
+echo "7. https://www.mediafire.com/file/uwj08ne93vfr89j/Index-2.html/file"
+sleep 1
+echo
+echo "8. https://www.mediafire.com/file/4ynpr2dbm9qitwf/1.html/file"
+sleep 1
+echo
+echo "9. https://www.mediafire.com/file/orx188pbrk6ohqa/Pazel.html/file"
+sleep 1
+echo
+echo "10. https://www.mediafire.com/file/2oez0f68pi74xfr/anjing.html/file"
+sleep 1
+echo
+echo "11. https://www.mediafire.com/file/crc5we1woiphqhg/sc1.html/file"
+sleep 1
+echo
+echo "12. https://www.mediafire.com/file/tj1yxejtde5g9jd/sc2.html/file"
+sleep 1
+echo
+echo "13. https://www.mediafire.com/file/sf21wb6bjd21cej/sc3.html/file"
+sleep 1
+echo
+echo "14. https://www.mediafire.com/file/tykt7xk5hayvbma/sc4.html/file"
+sleep 1
+echo
+echo "15. https://www.mediafire.com/file/8a4xj5c8vk9r203/sc5.html/file"
+sleep 1
+echo
+echo "16. https://drive.google.com/file/d/1qca2JOrqVwsBZk8vwKTheMDUADJniBSS/view"
+sleep 1
+echo
+echo "17. http://www.mediafire.com/file/qonh0fqynbpm7a6/1.html/file"
+sleep 1
+echo
+echo "18. http://www.mediafire.com/file/tzgp1qnrw01lg11/2.html/file"
+sleep 1
+echo
+echo "19. http://www.mediafire.com/file/ola4zjid04otsse/4.html/file"
+sleep 1
+echo
+echo "20. http://www.mediafire.com/file/r8q5pb9jw38g72u/6.html/file"
+echo
+sleep 2
+fi
